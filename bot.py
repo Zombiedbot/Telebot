2d09e40d53b43852351e492e89da601216085ccb
root <root@DESKTOP-MOK2TJQ.localdomain>
2019-08-25 16:31:50 +0300

    bot has sent me id of voice messages

--- bot.py
+++ bot.py
@@ -1,11 +1,18 @@
+import os
 import config
 import telebot
 
 bot = telebot.TeleBot(config.token)
 
-@bot.message_handler(content_types=["text"])
+@bot.message_handler(commands = ['test'])
 def repeat_all_message(message):
-    bot.send_message(message.chat.id, message.text)
+    for file in os.listdir('music/'):
+        if file.split('.')[-1] == 'mp3':
+            f = open('music/'+file, 'rb')
+            msg = bot.send_voice(message.chat.id, f, None) 
+            bot.send_message(message.chat.id, msg.voice.file_id, reply_to_message_id = msg.message_id)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
+
+
